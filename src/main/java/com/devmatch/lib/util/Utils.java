1ec8d754bd2fb3bc87adfd7f54a5314236bb0965
Gustavo Dalla <guhdalla011@gmail.com>
2023-02-28 17:11:19 -0300

    Removendo arquivos sem uso e corrigindo pom.xml

--- src/main/java/com/devmatch/lib/util/Utils.java
+++ src/main/java/com/devmatch/lib/util/Utils.java
@@ -1,10 +1,5 @@
 package com.devmatch.lib.util;
 
-import java.util.UUID;
-
 public class Utils {
 
-    public static String generateUUID() {
-        return UUID.randomUUID().toString();
-    }
 }
